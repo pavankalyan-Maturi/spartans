cf22aa8d932913fd8b6b6c5e565cc6735f2de4c5
pavankalyan-Maturi <70228172+pavankalyan-Maturi@users.noreply.github.com>
2020-09-02 15:01:18 +0530

    Update division.py

--- division.py
+++ division.py
@@ -2,7 +2,7 @@ divi=int(input("enter divi"))
 dive=int(input("enter dive"))
 val=0
 q=0
-for i in range(31,-1,-1):
+for i in range(31,-1,-1):  #-1 is taken in range to include 0
     if((val+(divi<<i))<=dive):
         val=val+(divi<<i)
         q=q|(1<<i)
