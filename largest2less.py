6d4e44f946accb8dbf8c34070b71f1d229e0c2ba
pavankalyan-Maturi <70228172+pavankalyan-Maturi@users.noreply.github.com>
2020-09-02 15:15:22 +0530

    Update largest2less.py

--- largest2less.py
+++ largest2less.py
@@ -1,9 +1,9 @@
 n=int(input("enter number"))
 h=0
-num=bin(n)[2: ]
-l=len(num)
+num=bin(n)[2: ] #to remove'ob'from the binary repreesentation
+l=len(num)      #after removing that then gives the crct length of no. of bits in binary
 for i in range(l-1):
-    h=h+(1<<i)
+    h=h+(1<<i)  #to create n-1 no. of set bits as per our question to ges 2 powers less...
 print(h+1,"is the largest power of 2 lss than",n)
 
 
