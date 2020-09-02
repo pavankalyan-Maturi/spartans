c21e9650604f133cfa86c7dceb7ed5fbf432dbbf
pavankalyan-Maturi <70228172+pavankalyan-Maturi@users.noreply.github.com>
2020-09-02 15:18:45 +0530

    Update msb.py

--- msb.py
+++ msb.py
@@ -1,5 +1,5 @@
 n=int(input("enter number"))
-num=bin(n)[2: ]
-l=len(num)
-h=1<<l-1
+num=bin(n)[2: ]  #to remove 'ob'from binary
+l=len(num)       #no. of binary bits 
+h=1<<l-1         #setting left most bit gives msb
 print(h,"is the msb")
